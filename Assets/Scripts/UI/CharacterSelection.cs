1e8fe88312e2ab15a36a38e21eb790398f4a6b78
Andrés Gabriel Fernández Romero <andresgabriel309@gmail.com>
2026-04-03 16:11:26 -0500

    [Patch] Communication between CharacterSelection & GameController

--- Assets/Scripts/UI/CharacterSelection.cs
+++ Assets/Scripts/UI/CharacterSelection.cs
@@ -1,20 +1,14 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour
 {
-    // This method will be called from the buttons
-    public void ChooseCharacter(int characterNumber)
+    // Methods to call from buttons
+    public void ChooseSara() => ChooseCharacter(Character.Sara);
+    public void ChooseMateo() => ChooseCharacter(Character.Mateo);
+    
+    // Private method to communicate with GameController
+    private void ChooseCharacter(Character character)
     {
-        if (characterNumber == 1)
-        {
-            Debug.Log("you selected character 1");
-            // Here you can add different logic for character 1
-        }
-        else if (characterNumber == 2)
-        {
-            Debug.Log("you selected character 2");
-            // Here you can add different logic for character 2
-        }
+        GameController.Instace.SetCurrentCharacter(character);
     }
 }
