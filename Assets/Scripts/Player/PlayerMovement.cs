32d6e5c85d64d2ffd9a37a60b4ddac1b7e8186b9
Andrés Gabriel Fernández Romero <andresgabriel309@gmail.com>
2026-04-04 12:53:23 -0500

    [Patch] Player Movement Behavior

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [Header("Movement Variables")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpForce;
+
+    // Interal variables
+    InputSystem_Actions actions;
+    Rigidbody2D rb;
+    float move;
+
+    // -------------------------- UNITY METHODS
+    void Awake()
+    {
+        actions = new InputSystem_Actions();
+    }
+    void OnEnable()
+    {
+        actions.Player.Enable();
+        // Move
+        actions.Player.Move.performed += PerformMovement;
+        actions.Player.Move.canceled += PerformMovement;
+        // Jump
+        actions.Player.Jump.performed += PerformJump;
+        actions.Player.Jump.canceled -= PerformJump;
+    }
+    void OnDisable()
+    {
+        actions.Player.Disable();
+        actions.Player.Move.performed -= PerformMovement;
+        actions.Player.Jump.performed -= PerformJump;
+    }
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        rb.velocity = new Vector2(move * moveSpeed * Time.deltaTime, rb.velocity.y);
+    }
+    // -------------------------- MOVEMENT METHODS
+    void PerformMovement(InputAction.CallbackContext ctx)
+    {
+        move = ctx.ReadValue<Vector2>().x;
+    }
+    void PerformJump(InputAction.CallbackContext ctx)
+    {
+        rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
     }
 }
