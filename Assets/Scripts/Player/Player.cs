144d278bcb2a4cc636f3774e4cfbccff25fd04d4
Andrés Gabriel Fernández Romero <andresgabriel309@gmail.com>
2026-04-10 16:05:48 -0500

    [Add] Player invoke events

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,10 +1,20 @@
+using System;
 using UnityEngine;
 
 public class Player : Being
 {
+    public static event Action<float> OnDamageTaken;
+    public static event Action OnPlayerDeath;
+
+    public override void TakeDamage(float damage)
+    {
+        base.TakeDamage(damage);
+        OnDamageTaken?.Invoke(CurrentHealth);
+    }
+
     protected override void Die()
     {
-        Debug.Log("Player died!");
+        OnPlayerDeath?.Invoke();
         // TODO: Handle death (game over screen, respawn, etc)
     }
 }
