4d59d8794150b105f79c98de1a7c51a250b88422
Andrés Gabriel Fernández Romero <andresgabriel309@gmail.com>
2026-04-10 19:38:41 -0500

    [Patch] Changed activation of elements from script

--- Assets/Scripts/UI/CharacterSelection.cs
+++ Assets/Scripts/UI/CharacterSelection.cs
@@ -1,16 +1,22 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterSelection : MonoBehaviour
 {
-    [SerializeField] GameObject preventInputPanel;
+    [SerializeField] List<GameObject> disableAfterConfirm;
+    [SerializeField] GameObject cheersTextBox;
 
     public void ChooseSara() => StartCoroutine(ConfirmSelection(Character.Sara));
     public void ChooseMateo() => StartCoroutine(ConfirmSelection(Character.Mateo));
 
     IEnumerator ConfirmSelection(Character character)
     {
-        preventInputPanel.SetActive(true);
+        foreach (GameObject obj in disableAfterConfirm)
+        {
+            obj.SetActive(false);
+        }
+        cheersTextBox.SetActive(true);
         yield return new WaitForSecondsRealtime(3.5f);
         GameController.Instace.SetCurrentCharacter(character);
     }
