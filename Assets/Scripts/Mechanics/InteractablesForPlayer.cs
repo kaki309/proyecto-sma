8aad5a5cac1f82102472979e82473e3c9fcaea98
Andrés Gabriel Fernández Romero <andresgabriel309@gmail.com>
2026-05-04 23:00:07 -0500

    [Add] Prefab

--- Assets/Scripts/Mechanics/InteractablesForPlayer.cs
+++ Assets/Scripts/Mechanics/InteractablesForPlayer.cs
@@ -4,31 +4,38 @@ using UnityEngine.UI;
 [RequireComponent(typeof(BoxCollider2D))]
 public abstract class InteractablesForPlayer : MonoBehaviour, IInteractable
 {
-
     public abstract void Interact();
-    [SerializeField] private GameObject interactionButton;
 
-    void Start()
+    [Header("World UI")]
+    [SerializeField] private GameObject interactionCanvas;
+
+    private Button button;
+
+    void Awake()
     {
-        // Set collider as trigger
         GetComponent<BoxCollider2D>().isTrigger = true;
-        // Disable Button Canvas
-        interactionButton.SetActive(false);
-        // Set click function on Button
-        interactionButton.GetComponentInChildren<Button>(true).onClick.AddListener(Interact);
+
+        button = interactionCanvas.GetComponentInChildren<Button>(true);
+
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(Interact);
+
+        interactionCanvas.SetActive(false);
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            interactionButton.SetActive(true);
+            interactionCanvas.SetActive(true);
         }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            interactionButton.SetActive(false);
+            interactionCanvas.SetActive(false);
         }
     }
 }
