fa2099bf19e88e1b397cadcb4f6a1332bac0600f
Andrés Gabriel Fernández Romero <andresgabriel309@gmail.com>
2026-05-11 14:13:14 -0500

    [Working] Light Switch

--- Assets/Scripts/Enemies/cite_startAttribute.cs
+++
@@ -1,5 +0,0 @@
-﻿using System;
-
-internal class cite_startAttribute : Attribute
-{
-}
--- Assets/Scripts/Mechanics/LightSwitch.cs
+++ Assets/Scripts/Mechanics/LightSwitch.cs
@@ -1,16 +1,29 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 public class LightSwitch : InteractablesForPlayer
 {
     [SerializeField] private float fearReductionAmount = 20f;
+    [SerializeField] Sprite[] animationFrames;
+    SpriteRenderer spriteRenderer;
 
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     public override void Interact()
     {
         if (FearManager.Instance != null)
         {
+            StartCoroutine(ChangeSprite());
             FearManager.Instance.ReduceFear(fearReductionAmount);
         }
     }
-
+    IEnumerator ChangeSprite()
+    {
+        foreach (Sprite sp in animationFrames)
+        {
+            spriteRenderer.sprite = sp;
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
 }
