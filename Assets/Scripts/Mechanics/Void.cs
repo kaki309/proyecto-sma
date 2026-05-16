47ee060f46c12d601b05cd11e1b2f3b91e1eaed4
Andrés Gabriel Fernández Romero <andresgabriel309@gmail.com>
2026-05-16 11:22:22 -0500

    [Patch] void logic

--- Assets/Scripts/Mechanics/Void.cs
+++ Assets/Scripts/Mechanics/Void.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class Void : MonoBehaviour
 {
-    [SerializeField] WorldSpawnPoints point;
+    [SerializeField] WorldSpawnPoints pointToSpawn;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            PlayerSpawner.Instance.SetCurrentSpawnPoint(point);
             collision.GetComponent<PlayerHealth>().TakeDamage();
-            PlayerSpawner.Instance.SpawnPlayerOnSpawnpoint(point);
+            PlayerSpawner.Instance.SpawnPlayerOnSpawnpoint(pointToSpawn);
         }
     }
 }
